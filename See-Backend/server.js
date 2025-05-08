289c869df2d8d563ef1d17ff25a883d7c245fc1e
heeenrik <heinrichmanng@gmail.com>
2025-05-08 18:12:57 +0200

    Backend Ordner erstellt und dateien hinzugefügt

---
+++ See-Backend/server.js
@@ -0,0 +1,43 @@
+// Imports //
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+
+// CORS setup //
+const corsConfig = {
+  origin: "http://127.0.0.1:5500/",
+  credentials: true,
+};
+
+// Server setup //
+const server = express();
+const port = 3000;
+
+server.use(cors(corsConfig));
+
+server.get("/", (req, resp) => {
+  resp.send("server loaded");
+});
+
+server.listen(port, () => {
+  console.log("server is listening on port " + port);
+});
+
+// MYSQL Intergration //
+const databaseConnection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "Datenbank",
+});
+
+// Connect to database
+databaseConnection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to database");
+});
+
+// API routes //
+server.get("/api/data", (req, resp) => {
+  resp.json({ message: "data from server" });
+});
